403f3375156367c472b35fb38a82c2cf904a17e8
celest <u11302201@o365.ttu.edu.tw>
2025-04-26 22:32:13 +0800

    second commit */選單，可選擇abc/*

--- 程式設計作業2.c
+++ 程式設計作業2.c
@@ -17,8 +17,32 @@ int main() {
     if (!checkPassword()) {
         printf("密碼錯誤超過三次，程式結束。\n");
         return 0;
-    }   
+    }
+
+    char choice;
+    while (1) {
+        system("cls");
+        showMenu();
+        scanf(" %c", &choice);
+
+        switch (choice) {
+            case 'a':
+            case 'A':
+                break;
+            case 'b':
+            case 'B':
+                break;
+            case 'c':
+            case 'C':
+                break;
+            default:
+                printf("無效的選項，請重新輸入。\n");
+                system("pause");
+        }
+    }
+    return 0;
 }
+
 
 void printWelcomeScreen() {
     printf("************************************\n");
@@ -61,5 +85,14 @@ int checkPassword() {
         }
     }
     return 0;
+}
+
+void showMenu() {
+    printf("--------------------------\n");
+    printf("| a. 畫出直角三角形       |\n");
+    printf("| b. 顯示乘法表           |\n");
+    printf("| c. 結束                 |\n");
+    printf("--------------------------\n");
+    printf("請選擇一個選項：");
 }
 
