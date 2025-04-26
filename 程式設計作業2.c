1965c43d1710fea0460e230c29a2b5c2821180f2
celest <u11302201@o365.ttu.edu.tw>
2025-04-26 22:21:04 +0800

    first commit */個人風格主頁跟密碼/*

---
+++ 程式設計作業2.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+#define PASSWORD 2025
+
+void printWelcomeScreen();
+int checkPassword();
+void showMenu();
+void drawTriangle();
+void showMultiplicationTable();
+void waitAndClear();
+char getValidChar(char min, char max);
+int getValidInt(int min, int max);
+
+int main() {
+    if (!checkPassword()) {
+        printf("密碼錯誤超過三次，程式結束。\n");
+        return 0;
+    }   
+}
+
+void printWelcomeScreen() {
+    printf("************************************\n");
+    printf("*                                  *\n");
+    printf("*         歡迎來到我的程式         *\n");
+    printf("*          E1B01  Program          *\n");
+    printf("*                                  *\n");
+    printf("************************************\n");
+    printf("*         Merry Christmas!         *\n");
+    printf("*                                  *\n");
+	printf("*                0                 *\n");
+	printf("*               000                *\n");
+	printf("*              00000               *\n");
+	printf("*             0000000              *\n");
+	printf("*            000000000             *\n");
+	printf("*           00000000000            *\n");
+	printf("*          0000000000000           *\n");
+	printf("*         000000000000000          *\n");
+	printf("*        00000000000000000         *\n");
+	printf("*       0000000000000000000        *\n");
+	printf("*      000000000000000000000       *\n");
+	printf("*              000000              *\n");
+	printf("*              000000              *\n");
+	printf("*              000000              *\n");
+	printf("************************************\n");
+    getch();
+    system("cls");
+}
+
+int checkPassword() {
+    int input, attempts = 0;
+    printWelcomeScreen();
+    while (attempts < 3) {
+        printf("請輸入4位數密碼：");
+        scanf("%d", &input);
+        if (input == PASSWORD) return 1;
+        else {
+            printf("密碼錯誤！\n");
+            attempts++;
+        }
+    }
+    return 0;
+}
+
