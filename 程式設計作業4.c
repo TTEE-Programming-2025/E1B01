8f6d47e3f5e9f1e9c1e60809f9b86c6ab3b8acc1
celest <u11302201@o365.ttu.edu.tw>
2025-05-24 20:31:00 +0800

    second commit */個人化頁面和密碼/*

--- 程式設計作業4.c
+++ 程式設計作業4.c
@@ -15,3 +15,51 @@ typedef struct {
 
 Student students[MAX_STUDENTS];
 int student_count = 0;
+
+void clearScreen() {
+    system("cls");
+}
+
+void pause() {
+    printf("\n請按任意鍵繼續...");
+    getchar();
+    getchar();
+}
+
+void printWelcomeArt() {
+    clearScreen();
+    printf("########################################\n");
+    printf("#                                        #\n");
+    printf("#                                        #\n");
+    printf("#                                        #\n");
+    printf("#     CCCCCCCCCCCCCCCCCCCCCCCCCCCC       #\n");
+    printf("#   CC                             CC    #\n");
+    printf("#  CC     *************************  CC  #\n");
+    printf("# CC     *                       *   CC  #\n");
+    printf("# CC     *        E1B01         *   CC   #\n");
+    printf("# CC     *                       *   CC  #\n");
+    printf("#  CC     *************************  CC  #\n");
+    printf("#   CC                             CC    #\n");
+    printf("#     CCCCCCCCCCCCCCCCCCCCCCCCCCCC       #\n");
+    printf("#                                        #\n");
+    printf("#                                        #\n");
+    printf("#                                        #\n");
+    printf("########################################\n");
+    printf("\n歡迎使用簡易成績系統，請輸入密碼 (4 位數):\n");
+}
+
+int login() {
+    int password, attempts = 0;
+    while (attempts < 3) {
+        printf("請輸入密碼: ");
+        scanf("%d", &password);
+        if (password == 2025) {
+            printf("\n密碼正確，歡迎進入系統！\n");
+            return 1;
+        } else {
+            printf("密碼錯誤！\n");
+            attempts++;
+        }
+    }
+    return 0;
+}
